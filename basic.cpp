b970385b75e9f7940371c56f6fa2b40f6519d614
Piyush Mittal <65077216+mittalpiyush06@users.noreply.github.com>
2022-09-25 13:20:23 +0530

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -2,13 +2,13 @@
 #include<bits/stdc++.h>
 #define int long long
 
+using namespace std;
+
 #ifdef PC
 #include "debug.h"
 #else
 #define db(...) 42
 #endif
-
-using namespace std;
 
 int32_t main(){
     ios_base::sync_with_stdio(false);
